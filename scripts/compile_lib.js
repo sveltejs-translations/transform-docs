d3ce28c7ae8bff2bbcdbbc106db6a9f456ddf0c3
AlexxNB <iam@alexxnb.ru>
2022-02-04 10:56:12 +0300

    add patch for svelte highlight

--- scripts/compile_lib.js
+++ scripts/compile_lib.js
@@ -17,6 +17,7 @@ async function clear(){
   await fetchRepoDir({src:'sveltejs/action-deploy-docs',dir: TMP_DIR});
 
   await patch_faq_title();
+  await patch_svelte_code_hl();
 
   console.log('Installing dependencies...');
   spawnSync('npm',['install'],{
@@ -48,5 +49,13 @@ async function patch_faq_title(){
   console.log('Patching FAQ title...');
   let body = await fs.readFile(file,'utf-8');
   body = body.replace('vFile.data.frontmatter.question','(vFile.data.frontmatter.question || vFile.data.frontmatter.title)');
+  await fs.writeFile(file,body);
+}
+
+async function patch_svelte_code_hl(){
+  const file = path.join(TMP_DIR,'src','format','code.ts');
+  console.log('Patching Svelte Code Highlighting...');
+  let body = await fs.readFile(file,'utf-8');
+  body = body.replace('sv = "svelte",','sv = "svelte",\nsvelte = "svelte",');
   await fs.writeFile(file,body);
 }
